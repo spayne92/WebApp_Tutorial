7357b0a47bd999c17a0123602cc5e9118c0072ea
spayne92 <sfpr43@mst.edu>
2020-12-26 18:30:46 -0600

    Logging Errors

--- DutchTreat/Data/DutchRepository.cs
+++ DutchTreat/Data/DutchRepository.cs
@@ -1,35 +1,70 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using Microsoft.Extensions.Logging;
 using DutchTreat.Data.Entities;
+
 
 namespace DutchTreat.Data
 {
     public class DutchRepository : IDutchRepository
     {
         private readonly DutchContext _ctx;
+        private readonly ILogger<IDutchRepository> _logger;
 
-        public DutchRepository(DutchContext ctx)
+        public DutchRepository(DutchContext ctx, ILogger<DutchRepository> logger)
         {
             _ctx = ctx;
+            _logger = logger;
         }
 
         public IEnumerable<Product> GetAllProducts()
         {
-            return _ctx.Products
-                .OrderBy(p => p.Title)
-                .ToList();
+            try
+            {
+                // Custom logging will come from DutchTreat namespace.
+                _logger.LogInformation("GetAllProducts was called.");
+
+                return _ctx.Products
+                    .OrderBy(p => p.Title)
+                    .ToList();
+            } 
+            catch (Exception ex)
+            {
+                _logger.LogInformation($"GetAllProducts failed: {ex}");
+
+                return null;
+            }
         }
 
         public IEnumerable<Product> GetProductsByCategory(string category)
         {
-            return _ctx.Products
-                .Where(p => p.Category == category)
-                .ToList();
+            try 
+            { 
+                return _ctx.Products
+                    .Where(p => p.Category == category)
+                    .ToList();
+            } 
+            catch (Exception ex)
+            {
+                _logger.LogInformation($"GetProductsByCategory failed: {ex}");
+
+                return null;
+            }
         }
 
         public bool SaveAll()
         {
-            return _ctx.SaveChanges() > 0;
+            try
+            {
+                return _ctx.SaveChanges() > 0;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation($"SaveAll failed: {ex}");
+
+                return false;
+            }
         }
     }
 }
