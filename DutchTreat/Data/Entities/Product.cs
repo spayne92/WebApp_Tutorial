cc281fec2c7f3ac6a28ce6aed50081ff066a84ad
spayne92 <sfpr43@mst.edu>
2020-12-28 16:28:34 -0600

    Fixing precision of entitiy decimals.

--- DutchTreat/Data/Entities/Product.cs
+++ DutchTreat/Data/Entities/Product.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DutchTreat.Data.Entities
 {
@@ -7,6 +8,7 @@ namespace DutchTreat.Data.Entities
     public int Id { get; set; }
     public string Category { get; set; }
     public string Size { get; set; }
+    [Column(TypeName = "decimal(18,2)")]
     public decimal Price { get; set; }
     public string Title { get; set; }
     public string ArtDescription { get; set; }
