4b0dad1f26a555313a2ea7302f4da95e5ccfc168
spayne92 <sfpr43@mst.edu>
2020-12-30 05:26:00 -0600

    Designing the Login View

---
+++ DutchTreat/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DutchTreat.Controllers
+{
+    public class AccountController : Controller
+    {
+        private object _logger;
+
+        public AccountController(ILogger<AccountController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Login()
+        {
+            // Checks if user is already logged in.
+            if (this.User.Identity.IsAuthenticated)
+            {
+                // Redirects to home page if already logged in.
+                return RedirectToAction("Index", "App");
+            }
+
+            return View();
+        }
+    }
+}
