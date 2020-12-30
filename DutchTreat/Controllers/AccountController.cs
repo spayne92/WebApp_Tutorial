b37d2c45de89357c2b01171ebe3005f0a22d1888
spayne92 <sfpr43@mst.edu>
2020-12-30 12:16:21 -0600

    Login and Logout Implementation

--- DutchTreat/Controllers/AccountController.cs
+++ DutchTreat/Controllers/AccountController.cs
@@ -1,15 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using DutchTreat.Data.Entities;
+using DutchTreat.ViewModels;
 
 namespace DutchTreat.Controllers
 {
     public class AccountController : Controller
     {
-        private object _logger;
+        private readonly ILogger<AccountController> _logger;
+        private readonly SignInManager<StoreUser> _signInManager;
 
-        public AccountController(ILogger<AccountController> logger)
+        public AccountController(ILogger<AccountController> logger, SignInManager<StoreUser> signInManager)
         {
             _logger = logger;
+            _signInManager = signInManager;
         }
 
         public IActionResult Login()
@@ -22,6 +29,38 @@ namespace DutchTreat.Controllers
             }
 
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Allows logging in without retreiving User entity.  
+                var result = await _signInManager.PasswordSignInAsync(model.Username, model.Password, 
+                    model.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    if (Request.Query.Keys.Contains("ReturnUrl"))
+                    {
+                        Redirect(Request.Query["ReturnUrl"].First());
+                    }
+                    return RedirectToAction("Shop", "App");
+                }
+            }
+
+            // First parameter blank, because issue with entire model, not specific prop.
+            ModelState.AddModelError("", "Failed to login.");
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "App");
         }
     }
 }
